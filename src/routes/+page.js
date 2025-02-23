ddd6c5e30f0cd019cac23b3b1d6f86cbbf3a7704
nikoKhab <nmangori.1@gmail.com>
2025-02-23 22:11:31 +0400

    data fetched

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,9 @@
-export async function load() {
-	const res = await fetch('https://fakestoreapi.com/products');
-	const products = await res.json();
+// src/routes/+page.js
+export async function load({ fetch }) {
+	const response = await fetch('https://fakestoreapi.com/products');
+	const data = await response.json();
 
 	return {
-		products
+		data
 	};
 }
-
-load();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,72 @@
 <script>
-    import { page } from "$app/state";
-let { data } = $props()
+
+	let { data } = $props()
+    console.log(data)
+    // products = data.data
+    console.log(data.data)
+    const products = data.data
+
 </script>
 
 <main>
-<ul>
-    {#each data as product }
-        <h1>{product.title}</h1>
-    {/each}
-</ul>
+    <ul>
+        {#each products as product}
+        <article class="product-card">
+            <img class="product-image" src={product.image} alt="">
+            <h2 class="product-title">{product.title}</h2>
+            <p class="product-price">{product.price}</p>
+            <a href="/product"><button class="product-button">view more</button></a>
+        </article>
+            
+        {/each}
+    </ul>
 </main>
 
 <style scoped>
-    
+    *{
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .product-card{
+        width: 500px;
+        height: 600px;
+        background-color: azure;
+        border: 2px solid grey;
+        padding: 10px;
+        margin: 20px;
+        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .product-image{
+        width: 300px;
+        height: auto;
+        max-height: 60%;
+        max-width: 50%;
+    }
+    .product-price{
+        opacity: 0.7;
+    }
+    .product-title{
+        opacity: 0.7;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+    .product-button{
+        width: 110px;
+        height: 50px;
+        background-color: rgba(0, 0, 255, 0.826);
+        font-size: 20px;
+        border: none;
+        border-radius: 10px;
+    }
+    .product-button:hover{
+        background-color: rgba(0, 0, 255, 0.457);
+    }
 </style>
